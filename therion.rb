82995bad81c8a6aa02cc85ed77e5e6963357c256
Afforix <plch.matej@gmail.com>
2025-11-17 21:40:19 +0100

    added testing pipeline, fixed brew test-bot warnings

--- therion.rb
+++ therion.rb
@@ -1,20 +1,19 @@
 class Therion < Formula
   desc "Processes survey data and generates maps or 3D models of caves"
   homepage "https://therion.speleo.sk"
-  url "https://github.com/therion/therion/archive/v6.3.4.tar.gz"
+  url "https://github.com/therion/therion/archive/refs/tags/v6.3.4.tar.gz"
   sha256 "e678a539aeab436465556e769fa9ecbc51b2944f8969a34b29cc3fdc6d78765a"
-  version "6.3.4"
   revision 1
-  head "https://github.com/therion/therion.git"
+  head "https://github.com/therion/therion.git", branch: "master"
 
-  depends_on "cmake" => :build
   depends_on "catch2" => :build
-  depends_on "fmt" => :build
+  depends_on "cmake" => :build
+  depends_on "fmt"
   depends_on "freetype"
   depends_on "ghostscript"
   depends_on "imagemagick"
+  depends_on "jpeg-turbo"
   depends_on "lcdf-typetools"
-  depends_on "libjpeg"
   depends_on "libpng"
   depends_on "pkg-config"
   depends_on "proj"
